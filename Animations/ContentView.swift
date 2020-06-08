536a90775e649fcd2a58bf7b14bbe02232d7ef82
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-06-09 08:36:19 +0900

    add Project 6, part 1

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,11 +9,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount = 0.0
+
     var body: some View {
-        Text("Hello, World!")
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
